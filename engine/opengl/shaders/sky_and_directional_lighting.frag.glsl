38ea7683defc185750047b2ea0d2f9e55d87c5a8
mbellman <mbellman@nwe.com>
2024-08-28 00:34:43 -0500

    using a temporary fake directional light source

--- engine/opengl/shaders/sky_and_directional_lighting.frag.glsl
+++ engine/opengl/shaders/sky_and_directional_lighting.frag.glsl
@@ -26,12 +26,25 @@ vec3 GetWorldPosition(float depth, vec2 frag_uv, mat4 inverse_projection, mat4 i
   return world_position.xyz;
 }
 
+vec3 GetRadianceFromLightSource(vec3 albedo, vec3 normal, vec3 position) {
+  // @temporary @todo pass as parameters
+  vec3 light_direction = vec3(-1.0, -1.0, -1.0);
+  vec3 light_color = vec3(1.0, 1.0, 0.5);
+
+  float I = dot(normal, -normalize(light_direction));
+
+  return albedo * light_color * I;
+}
+
 void main() {
   vec4 frag_color_and_depth = texture(in_color_and_depth, fragUv);
   vec4 frag_normal_and_material = texture(in_normal_and_material, fragUv);
 
+  vec3 albedo = frag_color_and_depth.rgb;
+  vec3 normal = normalize(frag_normal_and_material.xyz);
   vec3 position = GetWorldPosition(frag_color_and_depth.w, fragUv, inverse_projection_matrix, inverse_view_matrix);
+  vec3 color = GetRadianceFromLightSource(albedo, normal, position);
 
-  out_color_and_depth = vec4(frag_color_and_depth.rgb, frag_color_and_depth.w);
+  out_color_and_depth = vec4(color, frag_color_and_depth.w);
   // out_color_and_depth = vec4(position, frag_color_and_depth.w);
 }
