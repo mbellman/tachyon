21b81a10011b074db648a022f05b554231c03f32
mbellman <mbellman@nwe.com>
2024-10-05 03:25:00 -0500

    cosmodrone: world updates, space/stars background

--- engine/opengl/shaders/sky_and_directional_lighting.frag.glsl
+++ engine/opengl/shaders/sky_and_directional_lighting.frag.glsl
@@ -167,6 +167,78 @@ Material UnpackMaterial(uvec4 surface) {
   return Material(roughness, metalness, clearcoat, subsurface);
 }
 
+//
+// Description : GLSL 2D simplex noise function
+//      Author : Ian McEwan, Ashima Arts
+//  Maintainer : ijm
+//     Lastmod : 20110822 (ijm)
+//     License :
+//  Copyright (C) 2011 Ashima Arts. All rights reserved.
+//  Distributed under the MIT License. See LICENSE file.
+//  https://github.com/ashima/webgl-noise
+//
+vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
+
+float simplex_noise(vec2 v) {
+  // Precompute values for skewed triangular grid
+  const vec4 C = vec4(0.211324865405187,
+                      // (3.0-sqrt(3.0))/6.0
+                      0.366025403784439,
+                      // 0.5*(sqrt(3.0)-1.0)
+                      -0.577350269189626,
+                      // -1.0 + 2.0 * C.x
+                      0.024390243902439);
+                      // 1.0 / 41.0
+
+  // First corner (x0)
+  vec2 i  = floor(v + dot(v, C.yy));
+  vec2 x0 = v - i + dot(i, C.xx);
+
+  // Other two corners (x1, x2)
+  vec2 i1 = vec2(0.0);
+  i1 = (x0.x > x0.y)? vec2(1.0, 0.0):vec2(0.0, 1.0);
+  vec2 x1 = x0.xy + C.xx - i1;
+  vec2 x2 = x0.xy + C.zz;
+
+  // Do some permutations to avoid
+  // truncation effects in permutation
+  i = mod289(i);
+  vec3 p = permute(
+          permute( i.y + vec3(0.0, i1.y, 1.0))
+              + i.x + vec3(0.0, i1.x, 1.0 ));
+
+  vec3 m = max(0.5 - vec3(
+                      dot(x0,x0),
+                      dot(x1,x1),
+                      dot(x2,x2)
+                      ), 0.0);
+
+  m = m*m ;
+  m = m*m ;
+
+  // Gradients:
+  //  41 pts uniformly over a line, mapped onto a diamond
+  //  The ring size 17*17 = 289 is close to a multiple
+  //      of 41 (41*7 = 287)
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+  // Normalise gradients implicitly by scaling m
+  // Approximation of: m *= inversesqrt(a0*a0 + h*h);
+  m *= 1.79284291400159 - 0.85373472095314 * (a0*a0+h*h);
+
+  // Compute final noise value at P
+  vec3 g = vec3(0.0);
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * vec2(x1.x,x2.x) + h.yz * vec2(x1.y,x2.y);
+  return 130.0 * dot(m, g);
+}
+
 // @todo allow game-specific definitions
 vec3 GetSkyColor(vec3 sky_direction) {
   vec3 sun_direction = -directional_light_direction;
@@ -184,7 +256,31 @@ vec3 GetSkyColor(vec3 sky_direction) {
   float planet_atmosphere_alpha = clamp(pow(planet_dot, 40) * 20.0, 0, 1) * planet_atmosphere_sunlight_factor;
   vec3 planet_atmosphere_color = mix(vec3(0), planet_atmosphere_base_color, planet_atmosphere_alpha);
 
-  vec3 sky_color = planet_atmosphere_color + sun_color;
+  // @todo cleanup
+  float bg_noise = simplex_noise(sky_direction.xy);
+  bg_noise = clamp(bg_noise, 0.0, 1.0);
+
+  float bg_noise2 = simplex_noise(sky_direction.xz);
+  bg_noise2 = clamp(bg_noise2, 0.0, 1.0);
+
+  vec3 space_color = vec3(0);
+  space_color += vec3(bg_noise) * vec3(1, 0.5, 1) * 0.2;
+  space_color += vec3(bg_noise2) * vec3(0, 0.75, 1) * 0.1;
+
+  float stars_x = atan(sky_direction.x, sky_direction.z);
+  float stars_y = atan(length(sky_direction.xz), sky_direction.y);
+
+  float intensity =
+    120.0 *
+    (0.9 + 0.1 * sin(stars_x * 2.0)) *
+    (0.9 + 0.1 * cos(stars_y * 2.0));
+
+  float stars_noise = simplex_noise(vec2(stars_x, stars_y) * 200.0);
+  stars_noise = clamp(stars_noise, 0.0, 1.0);
+  stars_noise = pow(stars_noise, intensity) * 80.0;
+  vec3 stars_color = vec3(stars_noise);
+
+  vec3 sky_color = planet_atmosphere_color + sun_color + space_color + stars_color;
 
   return sky_color;
 }
