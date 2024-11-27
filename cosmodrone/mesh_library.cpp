f8ee5911cf3f8b2d1552b393d4bbd0d6935fb1bb
mbellman <mbellman@nwe.com>
2024-11-27 03:29:57 -0600

    cosmodrone: world updates

--- cosmodrone/mesh_library.cpp
+++ cosmodrone/mesh_library.cpp
@@ -40,6 +40,7 @@ static void LoadPlaceableMeshes(Tachyon* tachyon, State& state) {
   load_mesh(silo_3);
   load_mesh(silo_4);
   load_mesh(silo_5);
+  load_mesh(silo_6);
   load_mesh(torus_1);
   load_mesh(station_torus_1);
   load_mesh(station_torus_2);
@@ -194,6 +195,17 @@ static void LoadPlaceableMeshes(Tachyon* tachyon, State& state) {
       .scale = tVec3f(9000.f),
       .color = tVec3f(0.8f, 0.4f, 0.3f),
       .material = tVec4f(0.4f, 1.f, 0, 0.1f)
+    }
+  });
+
+  placeable_mesh_assets.push_back({
+    .mesh_name = "silo_6",
+    .mesh_index = meshes.silo_6,
+    .placeholder = true,
+    .defaults = {
+      .scale = tVec3f(40000.f),
+      .color = tVec3f(0.8f, 0.7f, 0.6f),
+      .material = tVec4f(0.5f, 1.f, 0, 0)
     }
   });
 
@@ -416,6 +428,9 @@ static void LoadGeneratedMeshes(Tachyon* tachyon, State& state) {
   load_mesh(silo_3_body);
   load_mesh(silo_3_frame);
 
+  load_mesh(silo_6_body);
+  load_mesh(silo_6_frame);
+
   load_mesh(girder_6_core);
   load_mesh(girder_6_frame);
 
@@ -474,6 +489,24 @@ static void LoadGeneratedMeshes(Tachyon* tachyon, State& state) {
       .defaults = {
         .color = tVec3f(0.2f),
         .material = tVec4f(0.2f, 0, 0, 0.3f)
+      }
+    },
+
+    // silo_6,
+    {
+      .mesh_index = meshes.silo_6_body,
+      .generated_from = meshes.silo_6,
+      .defaults = {
+        .color = tVec3f(0.8f, 0.7f, 0.6f),
+        .material = tVec4f(0.6f, 1.f, 0, 0)
+      }
+    },
+    {
+      .mesh_index = meshes.silo_6_frame,
+      .generated_from = meshes.silo_6,
+      .defaults = {
+        .color = tVec3f(0.5f),
+        .material = tVec4f(0.2f, 1.f, 0, 0)
       }
     },
 
