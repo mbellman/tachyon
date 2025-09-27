601a8292460a3f447dbdba4f8e53c83ef9ed72b3
mbellman <mbellman@nwe.com>
2025-09-26 20:19:03 -0500

    astro: spell light tweaks

--- astro/control_system.cpp
+++ astro/control_system.cpp
@@ -149,35 +149,21 @@ static void CastStun(Tachyon* tachyon, State& state) {
   auto& spells = state.spells;
 
   spells.last_stun_time = tachyon->running_time;
-
-  // Stun light
-  tachyon->point_lights.push_back({
-    .radius = 0.f,
-    .color = tVec3f(1.f, 0.8f, 0.4f),
-    .power = 5.f
-  });
-
-  // @todo use light ID and not index
-  spells.stun_light_id = tachyon->point_lights.size() - 1;
+  spells.stun_light_id = create_point_light();
 }
 
 static void CastHoming(Tachyon* tachyon, State& state) {
   auto& spells = state.spells;
 
   spells.last_homing_time = tachyon->running_time;
+  spells.homing_light_id = create_point_light();
+
+  auto& light = *get_point_light(spells.homing_light_id);
+
+  light.position = state.player_position;
 
   // @temporary
   spells.homing_target_entity.id = -1;
-
-  tachyon->point_lights.push_back({
-    .position = state.player_position,
-    .radius = 0.f,
-    .color = tVec3f(0.1f, 0.3f, 1.f),
-    .power = 5.f
-  });
-
-  // @todo use light ID and not index
-  spells.homing_light_id = tachyon->point_lights.size() - 1;
 }
 
 // @todo move to magic_system.cpp
@@ -191,12 +177,14 @@ static void HandleSpells(Tachyon* tachyon, State& state, const float dt) {
   // Stun spells
   {
     if (spells.stun_light_id != -1) {
-      auto& light = tachyon->point_lights[spells.stun_light_id];
+      auto& light = *get_point_light(spells.stun_light_id);
       float t = (tachyon->running_time - spells.last_stun_time) / 3.f;
       if (t > 1.f) t = 1.f;
 
       light.position = state.player_position + tVec3f(800.f, 1000.f, -800.f);
+      light.position.y += sqrtf(t) * 1200.f;
       light.radius = 25000.f * Tachyon_EaseInOutf(t);
+      light.color = tVec3f(1.f, 0.8f, 0.4f),
       light.power = 5.f * powf(1.f - t, 2.f);
     }
   }
@@ -204,10 +192,12 @@ static void HandleSpells(Tachyon* tachyon, State& state, const float dt) {
   // Homing spells
   {
     if (spells.homing_light_id != -1) {
-      auto& light = tachyon->point_lights[spells.homing_light_id];
+      auto& light = *get_point_light(spells.homing_light_id);
 
-      light.radius = 10000.f;
       light.position += tVec3f(0, 0, -5000.f) * dt;
+      light.radius = 10000.f;
+      light.color = tVec3f(0.1f, 0.3f, 1.f);
+      light.power = 5.f;
     }
   }
 }
