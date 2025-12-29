da0c2c70ca8e47c5794deb4597792a872f598eb4
mbellman <mbellman@nwe.com>
2025-12-28 23:21:25 -0600

    astro: relocating procedural tree code

--- astro/procedural_generation.cpp
+++ astro/procedural_generation.cpp
@@ -8,6 +8,7 @@
 #include "astro/collision_system.h"
 #include "astro/entity_behaviors/behavior.h"
 #include "astro/path_generation.h"
+#include "astro/procedural_trees.h"
 
 using namespace astro;
 
@@ -302,72 +303,6 @@ static void UpdateGround1Plants(Tachyon* tachyon, State& state) {
   {
     add_dev_label("  Total grass: ", std::to_string(total_grass));
   }
-}
-
-/**
- * ----------------------------
- * Tree mushrooms
- * ----------------------------
- */
-static void GenerateTreeMushrooms(Tachyon* tachyon, State& state) {
-  remove_all(state.meshes.tree_mushroom);
-
-  for (uint16 i = 0; i < 100; i++) {
-    create(state.meshes.tree_mushroom);
-  }
-}
-
-static void UpdateTreeMushrooms(Tachyon* tachyon, State& state) {
-  profile("UpdateTreeMushrooms()");
-
-  uint16 total_mushrooms = 0;
-
-  static Quaternion rotations[] = {
-    Quaternion::fromAxisAngle(tVec3f(0, 1.f, 0), 0.f),
-    Quaternion::fromAxisAngle(tVec3f(0, 1.f, 0), 1.f),
-    Quaternion::fromAxisAngle(tVec3f(0, 1.f, 0), 2.f),
-    Quaternion::fromAxisAngle(tVec3f(0, 1.f, 0), 3.f),
-    Quaternion::fromAxisAngle(tVec3f(0, 1.f, 0), 4.f)
-  };
-
-  static float scales[] = {
-    400.f,
-    300.f,
-    500.f,
-    320.f,
-    350.f
-  };
-
-  for_entities(state.oak_trees) {
-    auto& entity = state.oak_trees[i];
-
-    // @todo factor
-    if (abs(state.player_position.x - entity.position.x) > 20000.f) continue;
-    if (abs(state.player_position.z - entity.position.z) > 20000.f) continue;
-
-    if (state.astro_time > entity.astro_start_time + 200.f) {
-      for (int i = 0; i < 5; i++) {
-        auto& mushroom = objects(state.meshes.tree_mushroom)[total_mushrooms++];
-        float scale = scales[((i + entity.id)) % 5];
-
-        mushroom.position = entity.position;
-        mushroom.position.z += entity.visible_scale.x * 0.32f;
-        mushroom.position.y += 1000.f;
-        mushroom.position.y -= 300.f * float(i);
-        mushroom.position.x += fmodf(abs(entity.position.x) + 70.f * float(i), 300.f) - 150.f;
-
-        mushroom.rotation = rotations[i];
-        mushroom.scale = tVec3f(scale);
-
-        mushroom.color = tVec3f(0.9f, 0.6f, 0.3f);
-        mushroom.material = tVec4f(1.f, 0, 0, 0.4f);
-
-        commit(mushroom);
-      }
-    }
-  }
-
-  mesh(state.meshes.tree_mushroom).lod_1.instance_count = total_mushrooms;
 }
 
 /**
---
+++ astro/procedural_trees.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "astro/game_state.h"
+
+namespace astro {
+  namespace ProceduralGeneration {
+    void GenerateTreeMushrooms(Tachyon* tachyon, State& state);
+    void UpdateTreeMushrooms(Tachyon* tachyon, State& state);
+  }
+}
