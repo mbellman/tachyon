6a99aba78578e7e618b955d87f1e149fa4a172a4
mbellman <mbellman@nwe.com>
2026-03-23 22:39:06 -0500

    astro: groundwork for on-demand animated entities

--- astro/game.cpp
+++ astro/game.cpp
@@ -87,20 +87,37 @@ static void UpdateWaterPlane(Tachyon* tachyon, State& state) {
   state.water_level = water_plane.position.y;
 }
 
+// @todo AnimatedEntities::
 static void UpdateAnimatedEntities(Tachyon* tachyon, State& state) {
-  // @temporary
+  // Disable all animated entity meshes upfront
   for_range(0, MAX_ANIMATED_PEOPLE - 1) {
     auto& skin = state.person_skinned_meshes[i];
     auto& person = skinned_mesh(skin.mesh_index);
 
-    person.position = state.player_position + tVec3f(5000.f, float(i) * 1500.f, 0);
+    person.disabled = true;
+  }
+
+  // Use animated meshes on-demand based on proximity to entities
+  int32 next_index = 0;
+
+  // @todo factor
+  for_entities(state.lesser_guards) {
+    auto& entity = state.lesser_guards[i];
+
+    if (abs(state.player_position.x - entity.visible_position.x) > 15000.f) continue;
+    if (abs(state.player_position.z - entity.visible_position.z) > 15000.f) continue;
+
+    auto& skin = state.person_skinned_meshes[next_index++];
+    auto& person = skinned_mesh(skin.mesh_index);
+
+    person.position = entity.visible_position;
+    person.rotation = entity.visible_rotation;
     person.scale = tVec3f(1500.f);
     person.shadow_cascade_ceiling = 1;
+    person.disabled = false;
 
-    // @TEMPORARY!!!!!!!!!!!!!!!!!1
-    person.rotation = skinned_mesh(state.meshes.player_robes).rotation;
+    // @TEMPORARY!!!!!!!!!!!!!!
     person.current_pose = &state.player_mesh_animation.active_pose;
-    // person.current_pose = &skin.animation.rest_pose;// active_pose;
 
     commit(person);
   }
