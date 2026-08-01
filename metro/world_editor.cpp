abf7a2e9154f206c38e40a6bd3c741b2a927ac4f
mbellman <mbellman@nwe.com>
2026-07-31 21:13:55 -0500

    metro: more editor groundwork

--- metro/world_editor.cpp
+++ metro/world_editor.cpp
@@ -3,9 +3,26 @@
 
 using namespace metro;
 
-static struct EditorState {
+static enum SelectionType {
+  NOTHING_SELECTED = -1,
+  BICYCLE,
+  STATIC_ENTITY,
+  INTERACTIVE_ENTITY
+};
 
+static struct EditorState {
+  SelectionType selection_type = NOTHING_SELECTED;
+  void* selection = nullptr;
 } editor;
+
+static bool IsAnythingSelected() {
+  return editor.selection_type != NOTHING_SELECTED;
+}
+
+static void Deselect() {
+  editor.selection_type = NOTHING_SELECTED;
+  editor.selection = nullptr;
+}
 
 static void HandleFreeCameraControls(Tachyon* tachyon, State& state) {
   auto& camera = tachyon->scene.camera;
@@ -40,8 +57,22 @@ static void HandleFreeCameraControls(Tachyon* tachyon, State& state) {
 
     camera.rotation = camera.orientation.toQuaternion();
   }
+}
 
-  // @temporary
+static void HandleClickActions(Tachyon* tachyon, State& state) {
+  if (did_left_click_down()) {
+    // @temporary
+    editor.selection_type = BICYCLE;
+  }
+
+  if (did_right_click_down()) {
+    Deselect();
+  }
+}
+
+static void ShowSelectionGizmo(Tachyon* tachyon, State& state) {
+  auto& camera = tachyon->scene.camera;
+
   tVec3f p = camera.position + camera.orientation.getDirection() * 2000.f;
 
   EditorUtilities::ShowPositionGizmo(tachyon, state, p);
@@ -58,10 +89,17 @@ void WorldEditor::Update(Tachyon* tachyon, State& state) {
 
   if (is_window_focused()) {
     HandleFreeCameraControls(tachyon, state);
+    HandleClickActions(tachyon, state);
+
+    if (IsAnythingSelected()) {
+      ShowSelectionGizmo(tachyon, state);
+    }
   }
 }
 
 void WorldEditor::Close(Tachyon* tachyon, State& state) {
+  Deselect();
+
   state.is_editor_open = false;
 
   show_overlay_message("Leaving editor");
