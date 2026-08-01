6dd60880d43b98be34409e6f22828ed212044649
mbellman <mbellman@nwe.com>
2026-07-31 19:26:42 -0500

    metro: creating a separate editor utilities module

---
+++ metro/editor_utilities.cpp
@@ -0,0 +1,69 @@
+#include "metro/editor_utilities.h"
+#include "metro/debug.h"
+
+using namespace metro;
+
+void EditorUtilities::ShowPositionGizmo(Tachyon* tachyon, State& state, const tVec3f& position) {
+  const float line_thickness = 15.f;
+  const tVec3f cone_scale = tVec3f(40.f);
+
+  // Left vector
+  {
+    DebugLineConfig left_line = {
+      .position = position,
+      .vector = tVec3f(250.f, 0, 0),
+      .color = tVec3f(1.f, 0, 0),
+      .thickness = line_thickness
+    };
+
+    DebugConeConfig left_cone = {
+      .position = left_line.position + left_line.vector,
+      .scale = cone_scale,
+      .direction = tVec3f(1.f, 0, 0),
+      .color = tVec3f(1.f, 0, 0)
+    };
+
+    Debug::ShowDebugLine(tachyon, state, left_line);
+    Debug::ShowDebugCone(tachyon, state, left_cone);
+  }
+
+  // Up vector
+  {
+    DebugLineConfig up_line = {
+      .position = position,
+      .vector = tVec3f(0, 250.f, 0),
+      .color = tVec3f(0, 1.f, 0),
+      .thickness = line_thickness
+    };
+
+    DebugConeConfig up_cone = {
+      .position = up_line.position + up_line.vector,
+      .scale = cone_scale,
+      .direction = tVec3f(0, 1.f, 0),
+      .color = tVec3f(0, 1.f, 0)
+    };
+
+    Debug::ShowDebugLine(tachyon, state, up_line);
+    Debug::ShowDebugCone(tachyon, state, up_cone);
+  }
+
+  // Forward vector
+  {
+    DebugLineConfig forward_line = {
+      .position = position,
+      .vector = tVec3f(0, 0, -250.f),
+      .color = tVec3f(0, 0, 1.f),
+      .thickness = line_thickness
+    };
+
+    DebugConeConfig forward_cone = {
+      .position = forward_line.position + forward_line.vector,
+      .scale = cone_scale,
+      .direction = tVec3f(0, 0, -1.f),
+      .color = tVec3f(0, 0, 1.f)
+    };
+
+    Debug::ShowDebugLine(tachyon, state, forward_line);
+    Debug::ShowDebugCone(tachyon, state, forward_cone);
+  }
+}
---
+++ metro/editor_utilities.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "metro/game_state.h"
+
+namespace metro {
+  namespace EditorUtilities {
+    void ShowPositionGizmo(Tachyon* tachyon, State& state, const tVec3f& position);
+  }
+}
